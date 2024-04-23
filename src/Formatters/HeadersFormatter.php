177da17583da97494a641c041e65a6579ba3e800
kEERill <russia199797@mail.ru>
2024-04-23 12:13:04 +0300

    [add] Добавлены функции скрытия заголовок в логах

--- src/Formatters/HeadersFormatter.php
+++ src/Formatters/HeadersFormatter.php
@@ -4,10 +4,26 @@ namespace Keerill\HttpLogger\Formatters;
 
 class HeadersFormatter
 {
+    private array $visible = ['*'];
+
+    private array $except = [];
+
     public function __construct(
-        protected array $hiddenHeaders = ['authorization', 'cookie', 'set-cookie'],
+        protected array $hidden = ['authorization', 'cookie', 'set-cookie'],
         protected string $stub = '****'
     ) {
+    }
+
+    public function only(array $only = ['*']): self
+    {
+        $this->visible = array_map(fn(string $value) => trim(strtolower($value)), $only);
+        return $this;
+    }
+
+    public function except(array $except = []): self
+    {
+        $this->except = array_map(fn(string $value) => trim(strtolower($value)), $except);
+        return $this;
     }
 
     public function format(array $headers): array
@@ -17,7 +33,11 @@ class HeadersFormatter
         foreach ($headers as $header => $values) {
             $header = trim(strtolower($header));
 
-            if (in_array($header, $this->hiddenHeaders)) {
+            if (!$this->isVisible($header)) {
+                continue;
+            }
+
+            if (in_array($header, $this->hidden)) {
                 $values[0] = $this->stub;
             }
 
@@ -25,5 +45,14 @@ class HeadersFormatter
         }
 
         return $cleanedHeaders;
+    }
+
+    private function isVisible(string $header): bool
+    {
+        if ($this->visible == ['*'] || in_array($header, $this->visible)) {
+            return !in_array($header, $this->except);
+        }
+
+        return false;
     }
 }
---
+++ tests/Formatters/HeadersFormatterTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use Keerill\HttpLogger\Formatters\HeadersFormatter;
+
+it('can formatter headers', function (
+    string $header, string $value, ?string $expected, array $only = ['*'], array $except = []) {
+    $headersFormatter = (new HeadersFormatter())
+        ->only($only)
+        ->except($except);
+
+    $headerFormatted = $headersFormatter->format([$header => [$value]]);
+
+    expect($headerFormatted[$header] ?? null)
+        ->toBe($expected);
+})->with([
+    ['authorization', 'token', '****'],
+    ['set-cookie', 'coookies', '****'],
+    ['cookie', 'coookies', '****'],
+    ['visible-header', 'value', 'value', ['visible-header']],
+    ['not-visible-header', 'value', null, ['visible-header']],
+    ['excepted-visible-header', 'value', null, ['*'], ['excepted-visible-header']],
+    ['header', 'value', 'value']
+]);
